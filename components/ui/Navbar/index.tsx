b7a3b2c27635a99160d80aecc3e7c7dcb2bf3eae
Rigoberto Cepeda <riguxc@gmail.com>
2023-08-05 16:56:55 -0600

    Added layout home

---
+++ components/ui/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import { Col, Layout, Row } from 'antd';
+import React from 'react';
+
+export const Navbar = () => {
+	return (
+		<Row>
+			<Col xs={24}>
+				<Layout></Layout>
+			</Col>
+		</Row>
+	);
+};
