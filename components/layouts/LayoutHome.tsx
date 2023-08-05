feb44a50f5c57d591286937e8a2abbfb35947966
Rigoberto Cepeda <riguxc@gmail.com>
2023-08-05 17:54:04 -0600

    feat(ui): :sparkles: Added navbar page

--- components/layouts/LayoutHome.tsx
+++ components/layouts/LayoutHome.tsx
@@ -14,10 +14,8 @@ export const LayoutHome: FC<_props> = ({ children }) => {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className={styles.body}>
-				<Navbar />
-				{children}
-			</main>
+			<Navbar />
+			<main className={styles.body}>{children}</main>
 		</>
 	);
 };
